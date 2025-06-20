e115cacbfb7bfe3566c67027db1323c58cf974f1
Paradauks <161374658+Paradauks@users.noreply.github.com>
2025-06-20 11:20:24 +0200

    Add files via upload

---
+++ article1.js
@@ -0,0 +1,6 @@
+document.getElementById("achat").addEventListener("click", function(event) {
+  event.preventDefault(); // 
+  alert("Merci pour ton achat tu va etre debite de 1 000 000 000 000 000 $ tu peut etre depite");
+  window.location.href = this.href;
+});
+
