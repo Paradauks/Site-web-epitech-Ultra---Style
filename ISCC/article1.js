1a5b85acf284ee413935fdb290406f126ba8144a
Paradauks <161374658+Paradauks@users.noreply.github.com>
2025-06-20 11:19:28 +0200

    Delete ISCC directory

--- ISCC/article1.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("achat").addEventListener("click", function(event) {
-  event.preventDefault(); // 
-  alert("Merci pour ton achat tu va etre debite de 1 000 000 000 000 000 $ tu peut etre depite");
-  window.location.href = this.href;
-});
-
--- ISCC/connexion.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("connect").addEventListener("click", function(event) {
-  event.preventDefault(); // 
-  alert("Tu as bien reussi a te connecter tu va etre rediriger a l'acceuil");
-  window.location.href = this.href;
-});
